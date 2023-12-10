1c675e7fa27b100fa34f368c23d5715d816f4066
Roberto <roberto.v.arebalo@gmail.com>
2023-12-10 17:38:37 -0300

    traigo los datos del formulario de registro

--- js/app.js
+++ js/app.js
@@ -1,10 +1,23 @@
 import Usuario from "./classUsuario.js";
 
-const formularioDeRegistro = document.querySelector('form');
+const formularioDeRegistro = document.querySelectorAll("form");
+const usuarios = [];
+const crearUsuario = (e) => {
+  e.preventDefault();
 
-const crearUsuario =(e)=>{
-    e.preventDefault();
-    
-}
+  const nombreApellido = document.getElementById('nombreApellido').value;
+  const nombreUsuario = document.getElementById('nombreUsuario').value;
+  const email = document.getElementById('email').value;
+  const telefono = document.getElementById('telefono').value;
+  const domicilio = document.getElementById('domicilio').value;
+  const localidad = document.getElementById('ciudad').value;
+  const codigoPostal = document.getElementById('codigoPostal').value;
+  const contraseña = document.getElementById('contraseña').value;
 
-formularioDeRegistro.addEventListener('submit',crearUsuario);
+  const nuevoUsuario = new Usuario(false,nombreApellido,nombreUsuario,email,telefono,domicilio,localidad,codigoPostal,contraseña);
+  usuarios.push(nuevoUsuario);
+
+  console.log(usuarios[0]);
+};
+
+formularioDeRegistro[1].addEventListener("submit", crearUsuario);
