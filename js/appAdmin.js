df7920d268b6c499dea3832819b9346083066faf
Roberto <roberto.v.arebalo@gmail.com>
2023-12-15 19:22:57 -0300

    agrego un array con generos de peliculas para seleccionar

--- js/appAdmin.js
+++ js/appAdmin.js
@@ -1,8 +1,40 @@
-import Usuario from "./classPelicula.js";
+import Pelicula from "./classPelicula.js";
 const formularioPeliculaNueva = document.getElementsByClassName(
     "formularioModal"
   );
   const peliculas = JSON.parse(localStorage.getItem("peliculasKey")) || [];
+  const generosPeliculas = [
+    "Acción",
+    "Aventura",
+    "Fantasía",
+    "Terror",
+    "Comedia",
+    "Drama",
+    "Romance",
+    "Suspenso",
+    "Ciencia Ficción",
+    "Musical",
+    "Animación",
+    "Arte",
+    "Autor",
+    "Catástrofe",
+    "Divulgación Científica",
+    "Buddy-cops",
+    "Desastres",
+    "Artes Marciales",
+    "Espías",
+    "Capa y Espada",
+    "Piratas",
+    "Supervivencia",
+    "Alta Fantasía",
+    "Sword & Sorcery",
+    "Fantasía Contemporánea",
+    "Bangsian Fantasy",
+    "Histórico",
+    "Western",
+    "Policiaco",
+    "Negro"
+  ];
   
   const crearPelicula = (e) => {
     e.preventDefault();
@@ -10,9 +42,7 @@ const formularioPeliculaNueva = document.getElementsByClassName(
     const tituloPelicula = document.getElementById("inputTitulo");
     const generoPelicula = document.getElementById("inputGenero");
     const descripcionPelicula = document.getElementById("inputDescripcion");
-    console.log(`esto es el nombre de la pelicula ${tituloPelicula.value}`);
-    console.log(`esto es el genero ${generoPelicula.value}`);
-    console.log(`esto es la descripcion ${descripcionPelicula.value}`);
+  
  
   };
   
