37c1f59ffc30ebbbb9a1eededae11e0ee33aca56
Valentina Acosta <valuchi005@gmail.com>
2023-12-18 13:38:53 -0300

    script de NavAdmin en pagina de admin

--- js/appNavAdmin.js
+++ js/appNavAdmin.js
@@ -1,15 +1,1 @@
-const nuevoUsuario = true;
 
-
-function Usuario() {
-  return nuevoUsuario ? 'admin' : 'user';
-}
-const tipoDeUsuario = Usuario
-const itemAdmin = document.getElementById('itemAdmin');
-
-if (tipoDeUsuario === 'admin') {
-    itemAdmin.style.display = 'block';
-  } else {
-    itemAdmin.style.display = 'none';
-  }
-  
