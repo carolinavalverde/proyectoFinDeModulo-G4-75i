64c1cbcb95e4df8eb6a427d7c3cd24bedac8098b
Pablo Lopez Sardi <lopezsardip@gmail.com>
2023-12-20 09:43:15 -0300

    agregué al modal los campos de UUID y de Imagen url

--- js/appBotonUsuario.js
+++ js/appBotonUsuario.js
@@ -4,15 +4,15 @@ const estatusDelUsuarioPresente =
 let elementos = document.getElementsByClassName("nav-link");
 let urlActual = window.location.href.slice(-10);
 if(estatusDelUsuarioPresente !== "nadie"){
-    for (let i = 0; i < elementos.length; i++) {
-        if (elementos[i].textContent === "Administrador") {
-          elementos[i].textContent = estatusDelUsuarioPresente;
-      
-          elementos[i].classList.remove("d-none");
-      
-          elementos[i].setAttribute("href", "../pages/admin.html");
-        }
-      }
+  for (let i = 0; i < elementos.length; i++) {
+    if (elementos[i].textContent === "Administrador") {
+      elementos[i].textContent = estatusDelUsuarioPresente;
+  
+      elementos[i].classList.remove("d-none");
+  
+      elementos[i].setAttribute("href", "../pages/admin.html");
+    }
+  }
 }
 
 
