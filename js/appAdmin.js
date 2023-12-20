10192337fdf2eb3006802d129cfb0ec0b78faf1b
Pablo Lopez Sardi <lopezsardip@gmail.com>
2023-12-20 01:12:23 -0300

    solucioné bugs de la pagina admin en gral que no me dejaban seguir con el update del crud

--- js/appAdmin.js
+++ js/appAdmin.js
@@ -1,17 +1,26 @@
 import Pelicula from "./classPelicula.js";
 
 function cargarPeliculasGuardadas() {
-  for (let index = 0; index < peliculas.length; index++) {
-    agregarFilaTabla(peliculas[index]);
-  }
+  // for (let index = 0; index < peliculas.length; index++) {
+  //   agregarFilaTabla(peliculas[index],index);
+  // }
+  peliculas.map((pelicula,indice) => agregarFilaTabla(pelicula,indice));
 }
 
-// function numeroCodigo(numero) {
-//   let numeroConCeros = String(numero).padStart(6, "0");
-//   return numeroConCeros;
-// }
+let peliculaNueva = true;
 
-function agregarFilaTabla(pelicula) {
+const buttonNuevaPelicula = document.getElementById("buttonAgregarPelicula");
+buttonNuevaPelicula.addEventListener("click", () => {
+  peliculaNueva = true;
+  const botonModal = document.getElementById("buttonSubmitModal");
+  botonModal.innerHTML = "+ Agregar";
+
+  const tituloModal = document.getElementById("modalPeliculaLabel");
+  tituloModal.innerHTML = "Nueva película"
+});
+
+
+function agregarFilaTabla(pelicula,index) {
   const tabla = document
     .getElementById("tablaDePeliculas")
     .getElementsByTagName("tbody")[0];
@@ -24,7 +33,7 @@ function agregarFilaTabla(pelicula) {
   const celdaPublicada = nuevaFila.insertCell(4);
   const celdaAcciones = nuevaFila.insertCell(5);
 
-  celdaCodigo.innerHTML = pelicula.codigo;
+  celdaCodigo.innerHTML = String(index+1).padStart(4, "0");
   celdaTitulo.innerHTML = pelicula.titulo;
   celdaCategoria.innerHTML = pelicula.categoria;
   celdaDescripcion.innerHTML = pelicula.descripcion;
@@ -47,9 +56,8 @@ function agregarFilaTabla(pelicula) {
   editButton.classList.add("fa-solid", "fa-pen-to-square", "editButton", "mx-1");
   editButton.setAttribute("data-bs-toggle", "modal");
   editButton.setAttribute("data-bs-target", "#modalCreatePelicula");
-
   editButton.addEventListener("click", function () {
-    editarPelicula(pelicula);
+    cargarDatosEnFormulario(pelicula);
   });
 
   celdaAcciones.appendChild(editButton);
@@ -84,16 +92,16 @@ function eliminarPelicula(pelicula) {
     }
 
     guardarEnLocalStorage();
+    
   }
 }
 
 let codigoPelicula;
 
-function editarPelicula(pelicula){
-  cargarDatosEnFormulario(pelicula);
-}
 
 function cargarDatosEnFormulario(pelicula) {
+  peliculaNueva = false;
+
   const botonModal = document.getElementById("buttonSubmitModal");
   botonModal.innerHTML = "Actualizar";
 
@@ -112,42 +120,32 @@ function cargarDatosEnFormulario(pelicula) {
 
 }
 
-function actualizarPelicula(pelicula){
- 
+function actualizarPelicula(){
+  let posicion = peliculas.findIndex((pelicula) => pelicula.codigo === codigoPelicula);
+  
   const tituloPelicula = document.getElementById("inputTitulo");
   const numeroGeneroPelicula = document.getElementById("inputGenero");
   const descripcionPelicula = document.getElementById("inputDescripcion");
 
-  peliculas.forEach(pelicula => {
-    if (pelicula.codigo === codigoPelicula) {
-      const peliculaEncontrada = pelicula;
-      peliculaEncontrada.titulo = tituloPelicula.value;
-      peliculaEncontrada.genero = numeroGeneroPelicula.value;
-      peliculaEncontrada.descripcion = descripcionPelicula.value;
-    }
-  });
 
+  console.log(posicion);
+  console.log(peliculas[posicion]);
+  peliculas[posicion].titulo = tituloPelicula.value;
+  peliculas[posicion].genero = numeroGeneroPelicula.value;
+  peliculas[posicion].descripcion = descripcionPelicula.value;
   guardarEnLocalStorage();
+  $("#modalCreatePelicula").modal("hide");
 
-  const tabla = document
-    .getElementById("tablaDePeliculas")
-    .getElementsByTagName("tbody")[0];
-  const filas = tabla.getElementsByClassName("nueva-fila");
+  const tbody = document.getElementById("bodyTablaPeliculas");
+  tbody.children[posicion].children[1].innerHTML = tituloPelicula.value;
+  tbody.children[posicion].children[2].innerHTML = numeroGeneroPelicula.value;
+  tbody.children[posicion].children[3].innerHTML = descripcionPelicula.value;
 
-  for (let i = 0; i < filas.length; i++) {
-    const codigoFila = filas[i].getElementsByTagName("td")[0].innerHTML;
-      tabla.deleteRow(i);
-  }
 
-  cargarPeliculasGuardadas();
   codigoPelicula = null;
-
-  const botonModal = document.getElementById("buttonSubmitModal");
-  botonModal.innerHTML = "+ Agregar";
-
-  const tituloModal = document.getElementById("modalPeliculaLabel");
-  tituloModal.innerHTML = "Nueva película"
+  peliculaNueva = true;
 }
+
 
 
 function guardarEnLocalStorage() {
@@ -161,66 +159,37 @@ window.onload = cargarPeliculasGuardadas();
 const generosPeliculas = ["Acción", "Animadas", "Navideñas", "Románticas"];
 
 
-const crearPelicula = (e) => {
-  e.preventDefault();
-
+const crearPelicula = () => {
   const tituloPelicula = document.getElementById("inputTitulo");
-  const numeroGeneroPelicula = document.getElementById("inputGenero");
+  const generoPelicula = document.getElementById("inputGenero");
   const descripcionPelicula = document.getElementById("inputDescripcion");
 
-  if (true) {
-    const nuevaPelicula = new Pelicula(
-      tituloPelicula.value,
-      generosPeliculas[numeroGeneroPelicula.value],
-      descripcionPelicula.value
-    );
+  const nuevaPelicula = new Pelicula(
+    tituloPelicula.value,
+    generosPeliculas[numeroGeneroPelicula.value],
+    descripcionPelicula.value
+  );
 
-    peliculas.push(nuevaPelicula);
-    agregarFilaTabla(nuevaPelicula);
-    guardarEnLocalStorage();
-    $("#modalCreatePelicula").modal("hide");
-  } else {
-  }
+  peliculas.push(nuevaPelicula);
+  
+  agregarFilaTabla(nuevaPelicula,peliculas.length-1);
+  guardarEnLocalStorage();
+  $("#modalCreatePelicula").modal("hide");
+
 
   console.log(peliculas);
 };
 
 
-// const submitModalAction = (e) =>{
+formularioPelicula[0].addEventListener("submit", prepararFormulario);
 
-//   e.preventDefault();
-//   if(botonModal.innerHTML.includes("+ Agregar")){
-//     crearPelicula;
-//   } else if (botonModal.innerHTML.includes("Actualizar")){
-//     actualizarPelicula(this);
-//   }
-//   $("#modalCreatePelicula").modal("hide");
-// }
+function prepararFormulario(e){
+  e.preventDefault();
 
-// const botonModal = document.getElementById("buttonSubmitModal").value;
+  if(peliculaNueva){
+    crearPelicula();
+  }else{
+    actualizarPelicula();
+  }
+}
 
-formularioPelicula[0].addEventListener("submit", crearPelicula);
-// formularioPelicula[0].addEventListener("submit", actualizarPelicula(this));
-
-
-// function chooseFunction(){
-//   if(botonModal.includes("+ Agregar")){
-//     crearPelicula;
-//   } else if (botonModal.includes("Actualizar")){
-//     actualizarPelicula(this);
-//   }
-//   $("#modalCreatePelicula").modal("hide");
-// }
-
-
-
-
-// formularioPelicula[0].addEventListener("submit", function (e) {
-//   e.preventDefault(); 
-//   if(botonModal.innerHTML.includes("+ Agregar")){
-//     crearPelicula;
-//   } else if (botonModal.innerHTML.includes("Actualizar")){
-//     actualizarPelicula(this);
-//   }
-//   $("#modalCreatePelicula").modal("hide");
-// });
